a96617ad0a30bcdcb8d4abf40ded2ea81594754f
Gavin Cannon <104046105+gavin-cannon@users.noreply.github.com>
2024-05-15 17:48:34 -0600

    W04 Changes

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
-
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    {
+      id: 1,
+      name: 'Document One',
+      description: 'This is the first document.',
+      url: 'https://example.com/document-one',
+      children: []
+    },
+    {
+      id: 2,
+      name: 'Document Two',
+      description: 'This is the second document.',
+      url: 'https://example.com/document-two',
+      children: []
+    },
+    {
+      id: 3,
+      name: 'Document Three',
+      description: 'This is the third document.',
+      url: 'https://example.com/document-three',
+      children: []
+    },
+    {
+      id: 4,
+      name: 'Document Four',
+      description: 'This is the fourth document.',
+      url: 'https://example.com/document-four',
+      children: []
+    },
+    {
+      id: 5,
+      name: 'Document Five',
+      description: 'This is the fifth document.',
+      url: 'https://example.com/document-five',
+      children: []
+    }
+  ];
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
